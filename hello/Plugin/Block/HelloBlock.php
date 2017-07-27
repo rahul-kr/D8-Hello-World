e080f7af0d383d470971a4e94bb6b46f9a7e84f6
Rahul Kumar <rahul.kumar@srijan.net>
2017-07-27 13:43:04 +0530

    custom block

---
+++ hello/Plugin/Block/HelloBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\hello\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ *   category = @Translation("Hello World"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World! Its custom block.'),
+    );
+  }
+
+}
